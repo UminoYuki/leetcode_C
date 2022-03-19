e9ead4ea5372ac7120d15b223fa5b3dbf9490244
Yuki <1286060599@qq.com>
2022-03-19 10:43:23 +0800

    第一次提交

---
+++ leedcode/1518numWaterBottles.cpp
@@ -0,0 +1,17 @@
+// 1518.换酒问题
+// https://leetcode-cn.com/problems/water-bottles/
+#include<stdio.h>
+int numWaterBottles(int numBottles, int numExchange){
+    int m;
+    m = numBottles + numBottles/numExchange;
+    numBottles = numBottles%numExchange + numBottles/numExchange;
+    while(numBottles>=numExchange){
+        m = m + numBottles/numExchange;
+        numBottles = numBottles%numExchange + numBottles/numExchange;
+    }
+    return m;
+}
+int main(){
+    printf("%d",numWaterBottles(100,2));
+    // 199
+}
